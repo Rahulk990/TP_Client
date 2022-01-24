41ed0c019c82f857c92c0b5a0299e5cf193595b3
Rahul S <srahul@flock.com>
2022-01-24 15:56:49 +0530

    Adds Delta Implementation

--- src/utils/ContactManager.js
+++ src/utils/ContactManager.js
@@ -3,16 +3,19 @@ import {
   addContactAPI,
   deleteContactAPI,
   getContactsAPI,
+  getLatestIdAPI,
+  getLatestUpdatesAPI,
   updateContactAPI,
 } from "./APIUtils";
 import { useAuth } from "./contextUtils";
 import { removeLocalAuthTokens } from "./localStorageUtils";
 
 const syncManager = (fun) => {
-  return setInterval(fun, 60000);
+  return setInterval(fun, 10000);
 };
 
 const ContactManager = () => {
+  const [latestId, setLatestId] = useState(null);
   const [contactsList, setContactsList] = useState([]);
   const { authTokens, setAuthTokens } = useAuth();
 
@@ -24,21 +27,82 @@ const ContactManager = () => {
   const fetchContactList = useCallback(() => {
     getContactsAPI(authTokens)
       .then((res) => {
-        setContactsList(res);
+        if (res) {
+          setContactsList(res);
+        }
       })
       .catch((_) => {
         unauthorizedErrorHandler();
       });
   }, [authTokens, unauthorizedErrorHandler]);
 
+  const fetchLatestId = useCallback(() => {
+    getLatestIdAPI(authTokens)
+      .then((res) => {
+        if (res) {
+          setLatestId(res.value);
+          fetchContactList();
+        }
+      })
+      .catch((_) => {
+        unauthorizedErrorHandler();
+      });
+  }, [authTokens, fetchContactList, unauthorizedErrorHandler]);
+
+  const processTransactions = useCallback(
+    (transactions) => {
+      let updatedContactsList = contactsList.slice();
+      transactions.forEach((transaction) => {
+        const contact = JSON.parse(transaction.transactionString);
+        // console.log(contact);
+
+        if (contact.isDeleted) {
+          updatedContactsList = updatedContactsList.filter(
+            (cnt) => cnt.contactId !== contact.contactId
+          );
+        } else if (
+          updatedContactsList.findIndex(
+            (cnt) => cnt.contactId === contact.contactId
+          ) > -1
+        ) {
+          updatedContactsList = updatedContactsList.map((cnt) =>
+            cnt.contactId === contact.contactId ? contact : cnt
+          );
+        } else {
+          updatedContactsList = [...updatedContactsList, contact].sort((a, b) =>
+            ("" + a.fullName).localeCompare(b.fullName)
+          );
+        }
+      });
+
+      setContactsList(updatedContactsList);
+      setLatestId(transactions[transactions.length - 1].transactionId);
+    },
+    [contactsList]
+  );
+
+  const fetchLatestUpdates = useCallback(() => {
+    getLatestUpdatesAPI(authTokens, latestId)
+      .then((res) => {
+        if (res && res.length) {
+          processTransactions(res);
+        }
+      })
+      .catch((_) => {
+        unauthorizedErrorHandler();
+      });
+  }, [authTokens, latestId, processTransactions, unauthorizedErrorHandler]);
+
   const addContact = (contact) => {
     addContactAPI(contact, authTokens)
       .then((res) => {
-        setContactsList(
-          [...contactsList, res].sort((a, b) =>
-            ("" + a.fullName).localeCompare(b.fullName)
-          )
-        );
+        if (res) {
+          setContactsList(
+            [...contactsList, res].sort((a, b) =>
+              ("" + a.fullName).localeCompare(b.fullName)
+            )
+          );
+        }
       })
       .catch((_) => {
         unauthorizedErrorHandler();
@@ -72,12 +136,15 @@ const ContactManager = () => {
   };
 
   useEffect(() => {
-    fetchContactList();
-    let intervalId = syncManager(fetchContactList);
+    fetchLatestId();
+  }, [fetchLatestId]);
+
+  useEffect(() => {
+    let intervalId = syncManager(fetchLatestUpdates);
     return () => {
       clearInterval(intervalId);
     };
-  }, [fetchContactList]);
+  }, [fetchLatestUpdates]);
 
   return { contactsList, addContact, updateContact, deleteContact };
 };
